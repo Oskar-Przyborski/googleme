f3704580b7ef2e5ede9e9745759d734b6249cadd
Oskar-Przyborski <oskar.przski@gmail.com>
2022-12-20 16:04:46 +0100

    First commit (working version)

---
+++ go.mod
@@ -0,0 +1,10 @@
+module squar/googleme
+
+go 1.19
+
+require (
+	github.com/jwalton/go-supportscolor v1.1.0 // indirect
+	github.com/savioxavier/termlink v1.2.1 // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+	golang.org/x/term v0.0.0-20210220032956-6a3ed077a48d // indirect
+)
---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+
+	"github.com/savioxavier/termlink"
+)
+
+type ResponseType struct {
+	Items []ResponseItem `json:"items"`
+}
+type ResponseItem struct {
+	Title       string `json:"title"`
+	Link        string `json:"link"`
+	DisplayLink string `json:"displayLink"`
+	Snippet     string `json:"snippet"`
+}
+
+const API_KEY string = "AIzaSyAf4AXGBTAJNkk4i2_iUctk0sU-plFrNdA"
+const BROWSER_ID string = "24ff328fa6314445a"
+
+func getSearchUrl(query string) string {
+	return fmt.Sprintf("https://www.googleapis.com/customsearch/v1?key=%s&cx=%s&q=%s", API_KEY, BROWSER_ID, url.QueryEscape(query))
+}
+
+func main() {
+	query := strings.Join(os.Args[1:], " ")
+	url := getSearchUrl(query)
+	println("Searching for:", "\033[36m"+query+"\033[0m")
+	println()
+	response, err := http.Get(url)
+	if err != nil {
+		println("Error while getting response from google: ", err.Error())
+	}
+
+	var data ResponseType
+	json.NewDecoder(response.Body).Decode(&data)
+
+	if len(data.Items) == 0 {
+		println("Found 0 results ://")
+	} else {
+		for _, item := range data.Items {
+			println(item.DisplayLink)
+			println(termlink.ColorLink(item.Title, item.Link, "green"))
+			println(item.Snippet)
+			println()
+			println()
+		}
+	}
+}
